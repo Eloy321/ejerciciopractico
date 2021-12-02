67b1480c15e379aaaa6051467d9f830cedc1af21
elolop <elolop@ubuntu>
2021-12-02 17:28:09 +0100

    He hecho que muestre por pantalla la palabra "Bienvenide".

---
+++ ejercicio-practico/src/ejercicio/practico/EjercicioPractico.java
@@ -0,0 +1,16 @@
+
+package ejercicio.practico;
+import java.util.*;
+
+
+public class EjercicioPractico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     System.out.println("Bienvenide");
+     
+    }
+    
+}
