636295a4ec970a32e0256f3a2ef9f5a107cfb2b2
elolop <elolop@ubuntu>
2021-12-13 17:03:00 +0100

    He pedido que me introduzca la temperatura de n municipios y que muestre si a puesto un numero positivo o negativo y que muestre por pantalla la temperatura mayor y la menor

--- ejercicio-practico/src/ejercicio/practico/EjercicioPractico.java
+++ ejercicio-practico/src/ejercicio/practico/EjercicioPractico.java
@@ -1,7 +1,6 @@
-
 package ejercicio.practico;
-import java.util.*;
 
+import java.util.*;
 
 public class EjercicioPractico {
 
@@ -9,31 +8,44 @@ public class EjercicioPractico {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     System.out.println("Bienvenide");
-     float temp;
-     
-     
-     Scanner entrada =new Scanner(System.in);
-     System.out.print("Introduce el numerto de temperaturas que desea introducir:\n");
-      int n;
-      n=entrada.nextInt();
-     for(float i=1;i<=n;i++){
-       
-         
-     System.out.print("Introduce una temperatura:\n");
-     temp=entrada.nextFloat();
-        if (temp>0) {
-            System.out.println("Positivo");
+        System.out.println("Bienvenide");
+        float temp, tempma, tempme;
+        tempma = 0;
+        tempme = 0;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduce el numerto de temperaturas que desea introducir:\n");
+        int n;
+        n = entrada.nextInt();
+        for (float i = 1; i <= n; i++) {
+
+            System.out.print("Introduce una temperatura:\n");
+            temp = entrada.nextFloat();
             
+            if(i== 1){
+                tempme=temp;
+                tempma=temp;
+            } 
+            if(temp > 0) {
+                System.out.println("Positivo");
+
+            } else {
+                System.out.println("Negativo");
+
+            }
+            if (temp > tempma) {
+
+                tempma = temp;
+            }
+            if (temp < tempme) {
+                tempme = temp;
+
+            }
+
         }
-        else{ 
-        System.out.println("Negativo");
-        
-        
-        }
-     }
-     
-     
+
+        System.out.println("Mayor es igual    " + tempma + "  Menor es igual     " + tempme);
+
     }
-    
+
 }
