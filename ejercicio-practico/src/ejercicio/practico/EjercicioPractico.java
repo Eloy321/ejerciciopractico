6d483d15dddb8f95fef7ac733d924e92e0125a76
elolop <elolop@ubuntu>
2021-12-16 19:04:48 +0100

    He hecho que guarde todas las temperaturas en un array para mostrarlas todas.

--- ejercicio-practico/src/ejercicio/practico/EjercicioPractico.java
+++ ejercicio-practico/src/ejercicio/practico/EjercicioPractico.java
@@ -9,37 +9,43 @@ public class EjercicioPractico {
      */
     public static void main(String[] args) {
         System.out.println("Bienvenide");
-        float temp, tempma, tempme;
+        float tempma, tempme;
         //Float.MIN_VALUE representa el menor valor posible de Float y MAX_VALUE es el valor maximo que puede alcanzar los Float.
         tempma = Float.MIN_VALUE;
         tempme = Float.MAX_VALUE;
 
         Scanner entrada = new Scanner(System.in);
         System.out.print("Introduce el numero de temperaturas que desea introducir:\n");
-        int n;
+        int n, i = 0;
         n = entrada.nextInt();
-        for (int i = 1; i <= n; i++) {
+        float temp[] = new float[n];
+        for (i = 0; i < temp.length; i++) {
 
             System.out.print("Introduce una temperatura:\n");
-            temp = entrada.nextFloat();
-            
-            if(i== 1){
-                tempme=temp;
-                tempma=temp;
-            } 
-            if(temp >= 0) {
+            temp[i] = entrada.nextFloat();
+        }
+        for (i = 0; i < temp.length; i++) {
+            System.out.println(temp[i]);
+        }
+        for (i = 0; i < temp.length; i++) {
+            if (i == 1) {
+                tempme = temp[i];
+                tempma = temp[i];
+            }
+
+            if (temp[i] >= 0) {
                 System.out.println("Temperatura positiva");
 
             } else {
                 System.out.println("Temperatura negativa");
 
             }
-            if (temp > tempma) {
+            if (temp[i] > tempma) {
 
-                tempma = temp;
+                tempma = temp[i];
             }
-            if (temp < tempme) {
-                tempme = temp;
+            if (temp[i] < tempme) {
+                tempme = temp[i];
 
             }
 
